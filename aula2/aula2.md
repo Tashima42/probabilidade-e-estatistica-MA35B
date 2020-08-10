db3480157f132a322621509f2131b0eb8a182b80
Tashima42 <tashima42@pop-os.localdomain>
2020-08-10 12:20:56 -0300

    feat: more exercices to aula2

--- aula2/aula2.md
+++ aula2/aula2.md
@@ -6,3 +6,12 @@ Uma variável quantitativa pode ser descrita de diversas maneiras, e para tornar
 
 ### Medidas de tendência central
 São medidas ao redor das quais as observações das variáveis quantitativas tendem a se agrupas (média, mediana, etc)
+
+### Medidas de dispersão
+Apesar das medidas de tendência central oferecerem uma ótima possibilidade de entendimento da população, elas podem esconder informações muito valiosas e nesse momento as medidas de dispersão cumprem seu papel de complementar.
+    - Amplitude: Distância entre o maior e o menor valor do conjunto
+    - Desvio médio absoluto: Cálculo da distância média das observações em relação a média
+    - Desvio quadrático médio: Diferente do DMA, cada desvio é elevado ao quadrado para facilitar a determinação da função do valor absoluto.
+    - Variância e desvio-padrão populacionais: Uma boa opção quando é possível selecionar todos os indvíduos de uma população
+    - Variância e desvio padrão: Um estimador não viciado da variância quando não se tem acesso a toda a população.
+    - Intervalo interquartil: A diferença entre o primeiro e o terceiro quartis, uma forma de dividir a distribuição em quatro partes iguais, assim, o IIQ não é influenciado por valores discrepantes como o desvio padrão, porém, por si só não é muito confiável.
--- aula2/exercicios/ex1/ex1.r
+++ aula2/exercicios/ex1/ex1.r
@@ -1,4 +1,38 @@
 setwd("~/Documents/projects/UTFPR/probabilidade-e-estatistica-MA35B/aula2/exercicios/ex1")
-dados <- read.csv(file = "Dieta.csv", sep = ";", dec = ",")
-mediaPerda <- mean(dados$Perda)
-melhorDietaPorPerda <- tapply(dados$Perda, dados$Dieta, mean)
+dados <- read.csv(file = "Dieta.csv", sep = ";", dec = ".")
+
+# Definindo variáveis para as colunas de dados
+dadosDieta <- dados$Dieta
+dadosHipertensao <- dados$Hipertensao
+dadosPerda <- dados$Perda
+dadosIdade <- dados$Idade
+
+# Calculando a média de perda e qual a melhor dieta
+mediaPerda <- mean(dadosPerda)
+melhorDietaPorMediaPerda <- tapply(dadosPerda,dadosDieta,mean)
+medianaPerda <- median(dadosPerda)
+melhorDietaPorMedianaPerda <- tapply(dadosPerda, dadosDieta, median)
+diferencaEntreMaiorEMenorVetor <- diff(range(dadosPerda))
+maxMinDiferencaDieta <- tapply(dadosPerda, dadosDieta, range)
+maxMinDiferencaDietaA <- maxMinDiferencaDieta$A
+maxMinDiferencaDietaB <- maxMinDiferencaDieta$B
+diferencaDietaA <- diff(maxMinDiferencaDietaA)
+diferencaDietaB <- diff(maxMinDiferencaDietaB)
+desvioPadraoPerda <- sd(dadosPerda)
+desvioPadraoPerdaPorDieta <- tapply(dadosPerda, dadosDieta, sd)
+proporcaoPerdaPeso <- IQR(dadosPerda)
+proporcaoPerdaPesoPorDieta <- tapply(dadosPerda, dadosDieta, IQR)
+
+# Mostra os resultados no console
+mediaPerda
+melhorDietaPorMediaPerda
+medianaPerda
+melhorDietaPorMedianaPerda
+diferencaEntreMaiorEMenorVetor
+maxMinDiferencaDieta
+diferencaDietaA
+diferencaDietaB
+desvioPadraoPerda
+desvioPadraoPerdaPorDieta
+proporcaoPerdaPeso
+proporcaoPerdaPesoPorDieta
